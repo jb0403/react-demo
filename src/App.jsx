2a0c0ac81a3319a75f6dd57278ebf898f1a04edd
JB <jayeshb7548@gmail.com>
2021-01-17 08:44:38 +0530

    git commit -m "slot machine"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import Var from './Var'
+
+const SloM = (props) => {
+    // let x = '😄';
+    // let y = '😄';
+    // let z = '😄😁';
+    let {x,y,z} = props;
+
+    if ((x === y) && (y === z)) {
+        return (<>
+
+            <hr />
+            <div className="slot_inner">
+                <h1>{x}{y}{z}</h1>
+                <h1>IT'S MATCHED</h1>
+            </div>
+            <hr />
 
 
-function App() {
-    return (
-        <>
-        <Var/>
-        </>
-    );
+        </>);
+    } else {
+        return (<>
+            <hr />
+            <div className="slot_inner">
+                <h1>{x}{y}{z}</h1>
+                <h1>IT'S NOT MATCHED</h1>
+                <hr />
+            </div>
+        </>);
+    }
+}
+
+const App = () => {
+    return (<>
+        <h1 className="headingStyle">
+            🎰 Welcome to <span style={{ fontWeight: "bold" }}>Slot Machine Game</span>🎰
+        </h1>
+        <SloM x = '😄' y = '😄'  z = '😄'/>
+        <SloM x = '😄' y = '🤣'  z = '😄'/>
+        <SloM x = '😄' y = '😄'  z = '😄'/>
+    </>);
 }
 
 export default App;
+
